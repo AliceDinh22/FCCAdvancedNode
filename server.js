e0063af2fcb41b68cdfb9a69be7b4171078fadc5
Alice Dinh <dynlinh222@gmail.com>
2025-10-17 00:01:45 +0700

    h

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
+//const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -23,7 +23,6 @@ app.route('/').get((req, res) => {
 module.exports = app;
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
-
