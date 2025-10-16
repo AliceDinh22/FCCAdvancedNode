e9ca7ae55810124aee932184dc834dd5f640d5e5
Alice Dinh <dynlinh222@gmail.com>
2025-10-16 23:49:02 +0700

    f

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.route('/').get((req, res) => {
   });
 });
 
+module.exports = app;
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Listening on port ' + PORT);
