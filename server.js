0c6c00edc1c0c9661d4a6594fe7bc5d44be838ff
Alice Dinh <dynlinh222@gmail.com>
2025-10-17 00:31:44 +0700

    k

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 fccTesting(app); //For FCC testing purposes
-app.set('view engine', 'pug');
-app.set('views', './views/pug'); 
-
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 app.route('/').get((req, res) => {
-     res.render('index', {
-    title: 'Hello',
-    message: 'Please log in'
+  res.render('index', { 
+    title: 'FCC Advanced Node and Express',
+    message: 'Pug is working!'
   });
 });
-
-module.exports = app;
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
