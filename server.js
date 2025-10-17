0e5865fd16e67aaab4a96e58ccc66d532ab3b290
Alice Dinh <dynlinh222@gmail.com>
2025-10-17 13:21:30 +0700

    o

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const cors = require("cors");
+const session = require('express-session');
+const passport = require('passport');
+
 
 const app = express();
 fccTesting(app); //For FCC testing purposes
@@ -11,6 +14,15 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
