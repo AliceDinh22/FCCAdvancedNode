ce84d748a9bf701d0e1b1912669d8729d40b8586
Alice Dinh <dynlinh222@gmail.com>
2025-10-17 13:52:14 +0700

    z

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
